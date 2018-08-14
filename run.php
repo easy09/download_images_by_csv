a80bf29c069080badbacbd0910b59ce66ddcd8e8
will <will@cn09.com>
2018-08-14 17:29:35 +0800

    add the param prefix

--- run.php
+++ run.php
@@ -34,8 +34,7 @@ function CurlGet($url)
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_HEADER, false);
-//curl_setopt($curl, CURLOPT_REFERER,$url);
-    curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 6.0; SeaPort/1.2; Windows NT 5.1; SV1; InfoPath.2)");  //模拟浏览器访问
+    curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 6.0; SeaPort/1.2; Windows NT 5.1; SV1; InfoPath.2)");
     curl_setopt($curl, CURLOPT_COOKIEJAR, ‘cookie . txt’);
     curl_setopt($curl, CURLOPT_COOKIEFILE, ‘cookie . txt’);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -50,7 +49,7 @@ function CurlGet($url)
  * download
  * @param $csv
  */
-function downloadJpgFromCsv($csv)
+function downloadJpgFromCsv($csv, $prefix = '')
 {
     $root = __DIR__ . '/dir_' . $csv;
     if (!file_exists($root)) {
@@ -73,6 +72,13 @@ function downloadJpgFromCsv($csv)
                         chmod($path, 0777);
                     }
                     $imgname = $path . '/' . $newFileName;
+                    if (strtolower(substr($url, 0, 4)) != 'http') {
+                        if($prefix){
+                            $url = $prefix . $url;
+                        }else{
+                            exit("URL must start with http,may be you need to add the param -p");
+                        }
+                    }
                     $command = "php run.php -f $csv -i $imgname -u $url";
                     $comm[] = popen($command . " &", 'r');
                     $i++;
@@ -99,12 +105,18 @@ function downloadJpgFromCsv($csv)
 
 set_time_limit(0);
 ini_set('memory_limit', '4096M');
-$argv = getopt('f:u:i:');
-if (!isset($argv['f'])) {
-    exit("php run.php -f yourcsv.csv");
+$argv = getopt('f:p:u:i:');
+
+$file = isset($argv['f']) ? trim($argv['f']) : '';
+$url = isset($argv['u']) ? trim($argv['u']) : '';
+$imgname = isset($argv['i']) ? trim($argv['i']) : '';
+$prefix = isset($argv['p']) ? trim($argv['p']) : '';
+if (!$file) {
+    echo "-f csv file name \n";
+    echo "-p the url prefix \n";
+    echo "php run.php -f yourcsv.csv\n";
+    exit("php run.php -f yourcsv.csv -p http://www.yourwebsite.com/image_path");
 }
-$url = isset($argv['u']) ? $argv['u'] : NULL;
-$imgname = isset($argv['i']) ? $argv['i'] : NULL;
 if ($url && $imgname) {
     if (strtolower(substr($url, 0, 4)) != 'http') {
         exit("URL must start with http");
@@ -115,9 +127,14 @@ if ($url && $imgname) {
 if (strtolower(substr($argv['f'], -4)) != '.csv') {
     exit("only download by *.csv");
 }
-$file = trim($argv['f']);
 if (!file_exists($file)) {
     exit("$file does not exist!");
 }
+if ($prefix && strtolower(substr($prefix, 0, 4)) != 'http') {
+    exit("prefix must start with http");
+}
+if ($prefix) {
+    $prefix = substr($prefix, -1) == '/' ? $prefix : $prefix . '/';
+}
 echo "start download {$file}\n";
-downloadJpgFromCsv($file);
+downloadJpgFromCsv($file, $prefix);
